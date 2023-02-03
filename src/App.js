f83599f1883adb6f647139bec4cd368f26f66779
Psivakrishnadev <Psivakrishnadev@users.noreply.github.com>
2023-02-03 23:37:33 +0530

    Made Modifications

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import Cards from "./components/Cards";
-import Header from "./components/Header";
+import React from 'react';
+import Cards from './components/Cards';
+import Header from './components/Header';
 // import Github from "./components/Github";
 
 const App = () => {
-	return (
-		<div>
-			<Header />
-			<Cards />
-		</div>
-	);
+  return (
+    <div>
+      <Header />
+      <Cards />
+    </div>
+  );
 };
 
 export default App;
